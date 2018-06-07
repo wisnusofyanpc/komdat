418705d229f085c6c5d77d409c1cca8490b4122f
bungsidik <bungsidik27@gmail.com>
2018-06-07 15:42:25 +0700

    tugas final

---
+++ MUHAMMAD-AZWAR-SIDIK-P3E116019.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title> ARTIKEL PERBEDAAN STATISTIK, STATISTIKA, DAN MATEMATIKA </title>
+</head>
+<body bgcolor="Silver" text=#FFFFFF>
+<font face="Times New Roman" size="12" leftmargin="200" rightmargin="500"> ARTIKEL TENTANG PERBEDAAN STATISTIKA, STATISTIK, DAN MATEMATIKA
+<P Align="Justivy">
+<BR>A.	Perbedaan Antara Statistik Dan Statistika <BR>
+<BR>1.	Statistik
+<P Align="Justify">Statistik adalah kumpulan angka mengenai suatu permasalahan sehingga dapat memberikan suatu gambaran dari permasalahan tersebut. Jenis data statistic menurut cara memperoleh dibagi menjadi dua yaitu data primer dan data sekunder. Data primer adalah data yang secara langsung diambil melalui obyek penelitian oleh peneliti secara individu maupun kelompok. Misalnya wawancara langsung penonoton bioskop  XXI untuk penelitian pretense konsumen bioskop. Sedangkan data sekunder adalah data yang didapat secara tidak langsung dari obyek penelitian. Disini peneliti mendapat data yang sudah dikumpulkan oleh pihak lain melalui berbagai cara, baik komersial maupun tidak komersial. Misalnya peneliti menggunakan hasil riset dari surat kabar yang akan digunakan untuk penelitian statistiknya.<BR></P>
+2. Statistika
+<P Align="Justify">Statistika adalah suatu metode ilmiah yang mempelajari teknik pengumpulan, perhitungan, penggambaran, pengaturan data  disertai dengan penarikan kesimpulan yang valid dari penganalisaan yang dilakukan secara pembuatan keputusan secara rasional.<BR>
+Statistika berbeda dengan statistik, jika dalam statistic terdapat jenis pengambilan data namun di dalam statistika terdapat metode pengambilan sampel yang terdiri dari dua teknik, yaitu teknik sampling berdasarkan peluang dan teknik sampling tidak berdasarkan peluang. Teknik sampling berdasarkan peluang adalah teknik pengambilan sampel dengan setiap unit observasi yang terdapat dalam populasi tersebut memiliki kesempatan yang sama untuk dijadikan pilihan sampel. Sedangkan teknik sampling tidak berdasarkan peluang adalah teknik pengambilan sampel di mana setiap unit dalam populasi tidak memiliki kesempatan yang sama untuk terpilih menjadi sampel.</P>
+B. PerbedaanMatematika Dan Statistika
+<P Align="Justify">Menurut  Linda Gattuso dan Maria Gabriella Ottaviani dalam bukunya yang berjudul “Complementing Mathematical Thinking  and Statistical Thingking in School Methematics”, antara Matematika dan Statistika memiliki tiga perbedaan.
+<P Align="Justify">Pertama, Statistika lebih menekankan kepada penalaran induktif sedangkan Matematika cenderung menggunakan penalaran deduktif. Matematika dikatakan deduktif karena beranjak dari aksioma dan teorema sehingga memunculkan penalaran-penalaran, model-model dan bukti baru berdasarkan aksioma dan teorema yang telah ada sebelumnya. Statistika, dengan situasi dan data yang sama pula bisa memberikan cara menganalisis yang berbeda dan memunculkan kesimpulan yang berbeda pula. Hal itu membutuhkan penalaran induktif, bekerja dengan randomisasi/pengacakan, pengambilan kesimpulan yang sesuai dengan menginterpretasi hasil yang didapat.</P>
+<P Align="Justify">Kedua, Matematika menyajikan abstraksi sedangkan Statistika memberikan wawasan dengan penginterpretasian situasi nyata. Matematika merupakan ilmu yang abstrak, pada awalnya mungkin terkesan nyata tetapi pada akhirnya matematika akan cenderung abstrak. Sedangkan Statistika lebih cenderung ke kejadian nyata seperti untuk mengetahui berapa jumlah penduduk yang bekerja dan merasa puas dengan pekerjaannya,  mengetahui jumlah presentasi ikan yang ada di lautan,  kita tidak bisa menggunakan perhitungan yang tepat karena kita hanya bisa mengkira-kira/menginterpretasikan dari contoh kecil yang didapat/diambil.</P>
+<P Align="Justify">Ketiga, Matematika dan Statistika berbeda dalam penggunaan bilangan. Matematika melihat bilangan sebagai bagian dari operasi, generalisasi, dan abstraksi sedangkan Statistika memandang bilangan yang dihubungkan dengan situasi nyata, sehingga penting dalam pembuatan pemodelan dan mengambil penalaran serta keputusan.</P>
+</body>
+</html>
